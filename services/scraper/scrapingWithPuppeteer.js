f69134d9b3a15fab5681ece19f09459a96e83e48
Mani Carmel <manicarmelvalguena@gmail.com>
2024-08-14 17:16:27 +0100

    iterate over pages and retrieve section titles

--- services/scraper/scrapingWithPuppeteer.js
+++ services/scraper/scrapingWithPuppeteer.js
@@ -1,5 +1,29 @@
 import puppeteer, { Puppeteer } from 'puppeteer'
 
+
+export default async function scrapFromGoogle() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com/search?q=master+mql+fsdm');
+    const titles = await page.$$eval('h3', elements => {
+        return elements.map(el => el.textContent);
+    })
+    const nextpages =  await page.$$eval('a.fl', elements => {
+        return elements.map(el => el.href);
+    })
+    for (let index = 0; index < nextpages.length; index++) {
+        const nextpage = await browser.newPage();
+        await nextpage.goto(nextpages[index]);
+        const pagetitles = await nextpage.$$eval('h3', elements => {
+            return elements.map(el => el.textContent);
+        })
+        titles.push(...pagetitles);
+    }
+    console.log(titles);
+    await browser.close();
+};
+
+/*
 export default async function scrapFromGoogle() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -14,3 +38,4 @@ export default async function scrapFromGoogle() {
     }
     await browser.close();
 };
+*/
